ddaa0e8a26d47e7550e3b291fa6e670a101645d5
Dmitry <dmtzh8@gmail.com>
2026-03-09 20:21:50 -0400

    Custom action registration

---
+++ main.py
@@ -0,0 +1,22 @@
+if __name__ == "__main__":
+    import asyncio
+
+    from infrastructure.rabbitmq import config
+    
+    from actions.filtersuccessresponse import FilterSuccessResponseHandler
+    from actions.filterhtmlresponse import FilterHtmlResponseHandler
+    from actions.getcontentfromhtml import GetContentFromHtmlHandler
+    from actions.getlinksfromhtml import GetLinksFromHtmlHandler
+    from actions.requesturl import RequestUrlHandler
+
+    from customactionhandler import create_custom_action_registration_handler
+
+    app = config.create_faststream_app()
+    register_custom_action = create_custom_action_registration_handler(config.run_action, config.action_handler)
+    register_custom_action(RequestUrlHandler())
+    register_custom_action(FilterSuccessResponseHandler())
+    register_custom_action(FilterHtmlResponseHandler())
+    register_custom_action(GetContentFromHtmlHandler())
+    register_custom_action(GetLinksFromHtmlHandler())
+
+    asyncio.run(app.run())
