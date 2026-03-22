45002c3d21b322be801f78fe546d86725b33d5c3
Dmitry <dmtzh8@gmail.com>
2026-03-21 23:42:10 -0400

    Filter json response action

--- actions/filterjsonresponse.py
+++ actions/filterjsonresponse.py
@@ -6,6 +6,7 @@ from shared.action import ActionName
 from shared.completedresult import CompletedResult, CompletedWith
 from shared.pipeline.actionhandler import DataDto
 from shared.utils.parse import parse_from_dict
+from shared.utils.result import to_error_list, to_ok_list
 
 from customactionhandler import CustomActionHandlerWithoutConfig
 
@@ -17,7 +18,7 @@ class FilterJsonResponseInput:
     def from_dict(data: DataDto) -> Result['FilterJsonResponseInput', str]:
         content_type_res = parse_from_dict(data, "content_type", lambda content_type: content_type if isinstance(content_type, str) else None)
         content_res = parse_from_dict(data, "content", lambda content: content if isinstance(content, str) else None)
-        errs = [err for err in [content_type_res.swap().default_value(None), content_res.swap().default_value(None)] if err is not None]
+        errs = to_error_list(content_type_res, content_res)
         match errs:
             case []:
                 return Result.Ok(FilterJsonResponseInput(content_type_res.ok, content_res.ok))
@@ -30,7 +31,16 @@ class FilterJsonResponseHandler(CustomActionHandlerWithoutConfig[FilterJsonRespo
         return ActionName("filterjsonresponse")
     
     def validate_input(self, dto_list: list[DataDto]) -> Result[FilterJsonResponseInput, Any]:
-        return FilterJsonResponseInput.from_dict(dto_list[0])
+        if not dto_list:
+            return Result.Error("input data is missing")
+        data_res_list = list(map(FilterJsonResponseInput.from_dict, dto_list))
+        data_list = to_ok_list(*data_res_list)
+        match data_list:
+            case []:
+                errs = to_error_list(*data_res_list)
+                return Result.Error(", ".join(errs))
+            case _:
+                return Result.Ok(data_list[0])
     
     async def handle(self, input: FilterJsonResponseInput) -> CompletedResult:
         match input.content_type:
