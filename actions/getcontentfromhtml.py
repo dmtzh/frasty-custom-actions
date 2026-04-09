e8003469622ea757ee59313c7adbaeec1d0e3134
Dmitry <dmtzh8@gmail.com>
2026-04-08 23:57:31 -0400

    Update getcontentfromhtml.py

--- actions/getcontentfromhtml.py
+++ actions/getcontentfromhtml.py
@@ -3,8 +3,6 @@ import functools
 from typing import Any
 
 from expression import Result
-from expression.collections.block import Block
-from expression.extra.result.traversable import traverse
 from parsel import Selector
 
 from shared.action import ActionName
@@ -13,7 +11,7 @@ from shared.customtypes import Error
 from shared.pipeline.actionhandler import DataDto
 from shared.utils.asyncresult import ex_to_error_result
 from shared.utils.parse import parse_bool_str, parse_from_dict, parse_non_empty_str
-from shared.utils.result import to_ok_list, to_error_list
+from shared.utils.result import to_error_list
 
 from customactionhandler import CustomActionHandler
 
@@ -68,38 +66,31 @@ class GetContentFromHtmlHandler(CustomActionHandler[GetContentFromHtmlConfig, Ge
         return GetContentFromHtmlConfig.from_dict(raw_config)
     
     def validate_input(self, dto_list: list[DataDto]) -> Result[GetContentFromHtmlInput, Any]:
-        def from_dict(data: DataDto):
-            content_res = parse_from_dict(data, "content", lambda content: content if isinstance(content, str) else None)
-            return content_res.map(lambda _: data)
-        data_list = to_ok_list(*map(from_dict, dto_list))
-        return Result.Ok(data_list)
+        return Result.Ok(dto_list)
     
-    async def handle(self, config: GetContentFromHtmlConfig, input: GetContentFromHtmlInput) -> CompletedResult:
-        def css_get_all(selector: Selector):
-            match config.css_selector:
-                case None:
-                    return None
-                case _:
-                    return Result[list[str], Error].Ok(selector.css(config.css_selector).getall())
-        def regex_get_all(selector: Selector):
-            match config.regex_selector:
-                case None:
-                    return None
-                case _:
-                    return Result[list[str], Error].Ok(selector.re(config.regex_selector))
-        @ex_to_error_result(Error.from_exception)
-        def get_from_content(content: dict):
-            selector = Selector(text=content["content"])
-            matches_res = css_get_all(selector) or regex_get_all(selector) or Result.Error(Error("Selector not specified"))
+    async def handle(self, config: GetContentFromHtmlConfig, input_list: GetContentFromHtmlInput) -> CompletedResult:
+        def get_content_from_input(input: DataDto) -> list[DataDto]:
+            if "content" not in input:
+                return [input]
+            selector = Selector(text=input["content"])
+            match config.css_selector, config.regex_selector:
+                case None, None:
+                    raise ValueError("Selector not specified")
+                case None, regex_selector:
+                    matches = selector.re(regex_selector)
+                case css_selector, _:
+                    matches = selector.css(css_selector).getall()
             output_name = config.output_name or "content"
-            html_without_output_name = {k:v for k, v in content.items() if k != output_name}
-            res = matches_res.map(lambda matches: [html_without_output_name | {output_name:match} for match in matches])
-            return res
+            output_list = [input | {output_name: match} for match in matches]
+            return output_list
+        @ex_to_error_result(Error.from_exception)
+        def get_content_from_input_list() -> list[DataDto]:
+            return functools.reduce(lambda acc, curr: acc + get_content_from_input(curr), input_list, [])
         def ok_to_completed_result(result_data: list):
             return CompletedWith.Data(result_data) if result_data or config.return_empty_result else CompletedWith.NoData()
         def err_to_completed_result(err):
             return CompletedWith.Error(str(err))
         
-        contents_res = traverse(get_from_content, Block(input))
-        res = contents_res.map(lambda contents: functools.reduce(lambda acc, curr: acc + curr, contents, []))
-        return res.map(ok_to_completed_result).default_with(err_to_completed_result)
+        get_content_res = get_content_from_input_list()
+        return get_content_res.map(ok_to_completed_result).default_with(err_to_completed_result)
+        
